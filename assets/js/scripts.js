18bc24ae60080804765fafd9eca54409be04cc39
ash-uga <aaw97657@uga.edu>
2025-03-11 14:40:40 -0400

    Initial theme code from Frankel

---
+++ assets/js/scripts.js
@@ -0,0 +1,141 @@
+document.addEventListener("DOMContentLoaded", function() {
+	const navItems = document.querySelectorAll(".wp-block-navigation-item a");
+	const submenus = document.querySelectorAll(".wp-block-navigation-submenu");
+
+	// Helper function to normalize URLs (remove trailing slash and make lowercase)
+	function normalizeUrl(url) {
+		return url.replace(/\/+$/, "").toLowerCase();
+	}
+
+	const currentUrl = normalizeUrl(window.location.href);
+
+	// Highlight current menu item
+	navItems.forEach((item) => {
+		if (normalizeUrl(item.href) === currentUrl) {
+			item.classList.add("active-page");
+		}
+	});
+
+	// Highlight parent menu item if any child link is active
+	submenus.forEach(submenu => {
+		const childLinks = submenu.querySelectorAll(".wp-block-navigation-item a");
+		childLinks.forEach(link => {
+			if (normalizeUrl(link.href) === currentUrl) {
+				submenu.closest(".wp-block-navigation-item").classList.add("child-active");
+			}
+		});
+	});
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const sideNav = document.querySelector('.side-nav');
+  const footer = document.querySelector('.uga_footer ');
+  
+  // Calculate offset for the side navigation
+  const footerOffset = footer.offsetTop;
+
+  window.addEventListener('scroll', function () {
+	const scrollPosition = window.scrollY;
+	const sideNavHeight = sideNav.offsetHeight;
+	const windowHeight = window.innerHeight;
+
+	// When the user scrolls past the footer, make the side-nav "absolute"
+	  console.log("SideNavHEight", sideNavHeight);
+			console.log("scrollPosition", scrollPosition);
+	  console.log("footerOffset - sidenav: ", footerOffset - sideNavHeight);
+
+	if (scrollPosition + sideNavHeight >= footerOffset) {
+	  sideNav.style.position = 'absolute';
+	  sideNav.style.top = footerOffset - sideNavHeight + 35 + 'px';
+	} else {
+	  // Reset to fixed when above the footer
+	  sideNav.style.position = 'fixed';
+	  sideNav.style.top = '100px';
+	}
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const currentUrl = window.location.href;
+  const menuLinks = document.querySelectorAll('.vertical-menu a');
+
+  menuLinks.forEach(link => {
+	if (link.href === currentUrl) {
+	  link.classList.add('active');  // Highlight the current link
+	  const childMenu = link.closest('.child-links');
+	  if (childMenu) {
+		childMenu.style.display = 'block';  // Keep the menu open
+		const parentLink = childMenu.previousElementSibling;
+		const triangleIcon = parentLink.querySelector('.triangle');
+		if (triangleIcon) {
+		  triangleIcon.style.transform = 'rotate(180deg)';  // Rotate the arrow
+		}
+	  }
+	}
+  });
+});
+
+function toggleMenu(event) {
+  event.preventDefault();
+  const parentLink = event.target.closest('.parent');
+  const childMenu = parentLink.nextElementSibling;
+  const triangleIcon = parentLink.querySelector('.triangle');
+
+  if (childMenu && (childMenu.classList.contains('child-links') || childMenu.classList.contains('child-links-home'))) {
+	const isVisible = childMenu.style.display === 'block';
+
+	const allChildMenus = document.querySelectorAll('.child-links, .child-links-home');
+	const allTriangles = document.querySelectorAll('.triangle');
+
+	allChildMenus.forEach(menu => {
+	  menu.style.display = 'none';
+	});
+	allTriangles.forEach(triangle => {
+	  triangle.style.transform = 'rotate(0deg)';
+	});
+
+	if (!isVisible) {
+	  childMenu.style.display = 'block';
+	  triangleIcon.style.transform = 'rotate(180deg)';
+	}
+  }
+}
+
+/*// Select the elements
+const searchIcon = document.querySelector('.search_icon');
+const searchInput = document.querySelector('.search_input');
+const searchInput_white = document.querySelector('.search_input_white');
+
+// Initial width and opacity state
+let isOpen = false;
+
+// Add click event listener to the search icon
+searchIcon.addEventListener('click', function() {
+  if (isOpen) {
+	// If input is open, shrink the width to 0 and hide the input by setting opacity to 0
+	  if(searchInput) {
+			searchInput.style.width = '0';
+	searchInput.style.opacity = '0';
+	  } else {
+		  searchInput_white.style.width = '0';
+	searchInput_white.style.opacity = '0'; 
+	  }
+  
+	  
+  
+  } else {
+	// If input is closed, expand the width to 200px and set opacity to 1
+	  if(searchInput) {
+			searchInput.style.width = '40%';
+	searchInput.style.opacity = '1';
+	  } else {  
+	searchInput_white.style.width = '40%';
+	searchInput_white.style.opacity = '1';
+	  }
+  
+  }
+  // Toggle the state
+  isOpen = !isOpen;
+});*/
---
+++ functions.php
@@ -0,0 +1,264 @@
+<?php
+/**
+ * Frankel functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Frankel
+ * @since Frankel 1.0
+ */
+
+/**
+ * Register block styles.
+ */
+
+if ( ! function_exists( 'frankel_block_styles' ) ) :
+	/**
+	 * Register custom block styles
+	 *
+	 * @since Frankel 1.0
+	 * @return void
+	 */
+	function frankel_block_styles() {
+
+		register_block_style(
+			'core/details',
+			array(
+				'name'         => 'arrow-icon-details',
+				'label'        => __( 'Arrow icon', 'frankel' ),
+				/*
+				 * Styles for the custom Arrow icon style of the Details block
+				 */
+				'inline_style' => '
+				.is-style-arrow-icon-details {
+					padding-top: var(--wp--preset--spacing--10);
+					padding-bottom: var(--wp--preset--spacing--10);
+				}
+
+				.is-style-arrow-icon-details summary {
+					list-style-type: "\2193\00a0\00a0\00a0";
+				}
+
+				.is-style-arrow-icon-details[open]>summary {
+					list-style-type: "\2192\00a0\00a0\00a0";
+				}',
+			)
+		);
+		register_block_style(
+			'core/post-terms',
+			array(
+				'name'         => 'pill',
+				'label'        => __( 'Pill', 'frankel' ),
+				/*
+				 * Styles variation for post terms
+				 * https://github.com/WordPress/gutenberg/issues/24956
+				 */
+				'inline_style' => '
+				.is-style-pill a,
+				.is-style-pill span:not([class], [data-rich-text-placeholder]) {
+					display: inline-block;
+					background-color: var(--wp--preset--color--base-2);
+					padding: 0.375rem 0.875rem;
+					border-radius: var(--wp--preset--spacing--20);
+				}
+
+				.is-style-pill a:hover {
+					background-color: var(--wp--preset--color--contrast-3);
+				}',
+			)
+		);
+		register_block_style(
+			'core/list',
+			array(
+				'name'         => 'checkmark-list',
+				'label'        => __( 'Checkmark', 'frankel' ),
+				/*
+				 * Styles for the custom checkmark list block style
+				 * https://github.com/WordPress/gutenberg/issues/51480
+				 */
+				'inline_style' => '
+				ul.is-style-checkmark-list {
+					list-style-type: "\2713";
+				}
+
+				ul.is-style-checkmark-list li {
+					padding-inline-start: 1ch;
+				}',
+			)
+		);
+		register_block_style(
+			'core/navigation-link',
+			array(
+				'name'         => 'arrow-link',
+				'label'        => __( 'With arrow', 'frankel' ),
+				/*
+				 * Styles for the custom arrow nav link block style
+				 */
+				'inline_style' => '
+				.is-style-arrow-link .wp-block-navigation-item__label:after {
+					content: "\2197";
+					padding-inline-start: 0.25rem;
+					vertical-align: middle;
+					text-decoration: none;
+					display: inline-block;
+				}',
+			)
+		);
+		register_block_style(
+			'core/heading',
+			array(
+				'name'         => 'asterisk',
+				'label'        => __( 'With asterisk', 'frankel' ),
+				'inline_style' => "
+				.is-style-asterisk:before {
+					content: '';
+					width: 1.5rem;
+					height: 3rem;
+					background: var(--wp--preset--color--contrast-2, currentColor);
+					clip-path: path('M11.93.684v8.039l5.633-5.633 1.216 1.23-5.66 5.66h8.04v1.737H13.2l5.701 5.701-1.23 1.23-5.742-5.742V21h-1.737v-8.094l-5.77 5.77-1.23-1.217 5.743-5.742H.842V9.98h8.162l-5.701-5.7 1.23-1.231 5.66 5.66V.684h1.737Z');
+					display: block;
+				}
+
+				/* Hide the asterisk if the heading has no content, to avoid using empty headings to display the asterisk only, which is an A11Y issue */
+				.is-style-asterisk:empty:before {
+					content: none;
+				}
+
+				.is-style-asterisk:-moz-only-whitespace:before {
+					content: none;
+				}
+
+				.is-style-asterisk.has-text-align-center:before {
+					margin: 0 auto;
+				}
+
+				.is-style-asterisk.has-text-align-right:before {
+					margin-left: auto;
+				}
+
+				.rtl .is-style-asterisk.has-text-align-left:before {
+					margin-right: auto;
+				}",
+			)
+		);
+	}
+endif;
+
+add_action( 'init', 'frankel_block_styles' );
+
+/**
+ * Enqueue block stylesheets.
+ */
+
+if ( ! function_exists( 'frankel_block_stylesheets' ) ) :
+	/**
+	 * Enqueue custom block stylesheets
+	 *
+	 * @since Frankel 1.0
+	 * @return void
+	 */
+	function frankel_block_stylesheets() {
+		/**
+		 * The wp_enqueue_block_style() function allows us to enqueue a stylesheet
+		 * for a specific block. These will only get loaded when the block is rendered
+		 * (both in the editor and on the front end), improving performance
+		 * and reducing the amount of data requested by visitors.
+		 *
+		 * See https://make.wordpress.org/core/2021/12/15/using-multiple-stylesheets-per-block/ for more info.
+		 */
+		wp_enqueue_block_style(
+			'core/button',
+			array(
+				'handle' => 'frankel-button-style-outline',
+				'src'    => get_parent_theme_file_uri( 'assets/css/button-outline.css' ),
+				'ver'    => wp_get_theme( get_template() )->get( 'Version' ),
+				'path'   => get_parent_theme_file_path( 'assets/css/button-outline.css' ),
+			)
+		);
+	}
+endif;
+
+add_action( 'init', 'frankel_block_stylesheets' );
+
+/**
+ * Register pattern categories.
+ */
+
+if ( ! function_exists( 'frankel_pattern_categories' ) ) :
+	/**
+	 * Register pattern categories
+	 *
+	 * @since Frankel 1.0
+	 * @return void
+	 */
+	function frankel_pattern_categories() {
+
+		register_block_pattern_category(
+			'frankel_page',
+			array(
+				'label'       => _x( 'Pages', 'Block pattern category', 'frankel' ),
+				'description' => __( 'A collection of full page layouts.', 'frankel' ),
+			)
+		);
+	}
+endif;
+
+add_action( 'init', 'frankel_pattern_categories' );
+
+function mytheme_remove_color_palette() {
+    // Remove the default Gutenberg color palette
+    remove_theme_support('editor-color-palette');
+}
+add_action('after_setup_theme', 'mytheme_remove_color_palette');
+
+// Load scripts 
+add_action('init', 'header_scripts'); // Add Custom Scripts to wp_head
+function header_scripts() {
+	if ($GLOBALS['pagenow'] != 'wp-login.php' && !is_admin()) {
+		wp_register_script('scripts', get_template_directory_uri() . '/assets/js/scripts.js', array(), '1.0.0'); // Custom scripts
+		wp_enqueue_script('scripts'); // Enqueue it!
+	}
+}
+
+// Load styles
+add_action('wp_enqueue_scripts', 'header_styles'); // Add Child Theme Stylesheet
+function header_styles() {
+    wp_register_style('custom', get_stylesheet_directory_uri() . '/assets/css/custom.css', array(), '1.0', 'all');
+    wp_enqueue_style('custom'); // Enqueue it!
+
+}
+
+
+// Allow SVG
+add_filter( 'wp_check_filetype_and_ext', function($data, $file, $filename, $mimes) {
+
+  global $wp_version;
+  if ( $wp_version !== '4.7.1' ) {
+     return $data;
+  }
+
+  $filetype = wp_check_filetype( $filename, $mimes );
+
+  return [
+      'ext'             => $filetype['ext'],
+      'type'            => $filetype['type'],
+      'proper_filename' => $data['proper_filename']
+  ];
+
+}, 10, 4 );
+
+function cc_mime_types( $mimes ){
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter( 'upload_mimes', 'cc_mime_types' );
+
+function fix_svg() {
+  echo '<style type="text/css">
+        .attachment-266x266, .thumbnail img {
+             width: 100% !important;
+             height: auto !important;
+        }
+        </style>';
+}
+add_action( 'admin_head', 'fix_svg' );
